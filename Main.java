33b69eb8bfddb44a0e7a127d0deee1b44146b531
Dan Weese <Daniel.r.weese@gmail.com>
2019-10-28 11:40:12 -0500

    Update

--- Main.java
+++ Main.java
@@ -1,13 +1,27 @@
-import java.util.*; 
+import java.util.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent; 
 
 public class Main {
     
     public static void main(String[] args) {
+        int lastMinute = 59;
+        int thisMinute = 1;
+        GregorianCalendar date = new GregorianCalendar();
+        boolean keepLooping = true;
 
-        
-        MyWindow myWindow = new MyWindow("Glamper-Comm");
-        myWindow.setVisible(true);
 
+        while (keepLooping){
+            if (lastMinute != thisMinute){
+                MyWindow myWindow = new MyWindow("Clock");
+                myWindow.setVisible(true);
+            
+            }
+            
+            lastMinute = thisMinute;
+            thisMinute = date.get(Calendar.MINUTE); 
+            
+        }   
     }
-}
 }
